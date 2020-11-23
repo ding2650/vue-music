472583184b668d389f64a500b731828e5cd7e2f6
Dinglh <1808656324@qq.com>
2020-11-23 23:15:49 +0800

    playList

--- src/views/PlayList.vue
+++ src/views/PlayList.vue
@@ -1,50 +1,82 @@
 <template>
   <section class="playList-container fix">
-    <div class="header">
-      <div class="bg">
-        <img class="bg-img" :src="infos.coverImgUrl" alt="" />
+    <main ref="bs" v-height>
+      <div class="header">
+        <div class="bg">
+          <img class="bg-img" :src="infos.coverImgUrl" alt="" />
+        </div>
+        <div class="content">
+          <header>
+            <Back style="transform: translateY(0.05rem)" />
+            <h3>歌单</h3>
+          </header>
+          <section class="top-header">
+            <section class="cover-img">
+              <img class="full" :src="infos.coverImgUrl" alt="" />
+              <span class="count">{{ formatCount(infos.playCount) }}</span>
+            </section>
+            <section class="right">
+              <p class="title">{{ infos.name }}</p>
+              <div class="author">
+                <img
+                  class="img"
+                  :src="infos.creator && infos.creator.avatarUrl"
+                  alt=""
+                />
+                <span class="bar">
+                  {{ infos.creator && infos.creator.nickname }}
+                </span>
+              </div>
+            </section>
+          </section>
+          <ul class="ul" ref="content">
+            <li class="li">
+              <van-icon name="chat-o" size="22" />
+              <span class="span"> 评论</span>
+            </li>
+            <li class="li">
+              <van-icon name="like-o" size="22" />
+              <span class="span"> 点赞</span>
+            </li>
+            <li class="li">
+              <van-icon name="add-o" size="22" />
+              <span class="span"> 收藏</span>
+            </li>
+            <li class="li">
+              <van-icon name="ellipsis" class="rot" size="22" />
+              <span class="span"> 更多</span>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="content">
-        <header>
-          <Back style="transform: translateY(0.05rem)" />
-          <h3>歌单</h3>
+      <div class="list">
+        <header class="header">
+          <section class="list-left">
+            <van-icon
+              style="margin-right: 0.12rem"
+              name="play-circle-o"
+              size=".5rem"
+            />
+            播放全部<span class="span">(共3首)</span>
+          </section>
+          <section class="list-right">
+            <van-icon name="plus" style="margin-right: 0.08rem" size=".36rem" />
+            收藏（0.8）万
+          </section>
         </header>
-        <section class="top-header">
-          <section class="cover-img">
-            <img class="full" :src="infos.coverImgUrl" alt="" />
-            <span class="count">{{ formatCount(infos.playCount) }}</span>
-          </section>
-          <section class="right">
-            <p class="title">{{ infos.name }}</p>
-            <div class="author">
-              <img class="img" :src="infos.creator&&infos.creator.avatarUrl" alt="" />
-              <span class="bar">
-                {{infos.creator&& infos.creator.nickname }}
-              </span>
+        <ul class="songs">
+          <li class="songs-item" v-for="(item, i) in list" :key="i">
+            <div class="order">{{ i + 1 }}</div>
+            <div class="info">
+              <p class="titel">{{ item.name }}</p>
+              <p class="singer">
+                {{ formatAuthor(item.ar) + "-" + item.al.name }}
+              </p>
             </div>
-          </section>
-        </section>
-        <ul class="ul">
-          <li class="li">
-            <van-icon name="chat-o" size="22" />
-            <span class="span"> 评论</span>
-          </li>
-          <li class="li">
-            <van-icon name="like-o" size="22" />
-            <span class="span"> 点赞</span>
-          </li>
-          <li class="li">
-            <van-icon name="add-o" size="22" />
-            <span class="span"> 收藏</span>
-          </li>
-          <li class="li">
-            <van-icon name="ellipsis" class="rot" size="22" />
-            <span class="span"> 更多</span>
           </li>
         </ul>
       </div>
-    </div>
-    <div class="list"></div>
+    </main>
   </section>
 </template>
 
@@ -52,27 +84,53 @@
 import Back from "@com/common/Back";
 import { formatCount } from "@tools/tools";
 import { playList } from "@api/server";
+import BS from "better-scroll";
 export default {
   components: {
     Back,
   },
+  directives:{
+    'height':{
+      bind(el){
+        setTimeout(() => {
+         const dom = document.getElementsByClassName('songs')[0]
+         console.dir(dom)
+
+        }, 0);
+        
+      },
+    }
+  },
   data() {
     return {
       infos: {},
+      list: [],
+      scroll:null
     };
   },
+  mounted() {
+    this.initScoll()
+  },
   methods: {
+    initScoll () {
+      this.scroll = new BS(this.$refs.bs)
+      console.log(this.scroll)
+    },
+    formatAuthor(arr) {
+      if (arr.length === 1) return arr[0].name;
+      return arr.reduce((cur, nex, i) => {
+        return cur + nex.name + (i === arr.length - 1 ? "" : " / ");
+      }, "");
+    },
     getList() {
       playList({ id: this.$route.params.id }).then((res) => {
-        console.log(res);
         this.infos = res.data.playlist;
+        this.list = res.data.playlist.tracks;
       });
     },
     formatCount,
   },
-  created() {
-    console.log(111);
-  },
+
   activated() {
     this.getList();
   },
@@ -80,15 +138,76 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-.playList-container{
-  z-index: 9;
+.songs-item {
+  display: flex;
+  align-items: center;
+  .order {
+    padding: 0.52rem;
+  }
+  .info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    .singer {
+      color: #999;
+      margin-top: 0.05rem;
+      font-size: 0.32rem;
+      max-width: calc(100vw - 1.56rem);
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+  }
 }
+.playList-container {
+  z-index: 9;
+  background: var(--bg);
+}
+.list {
+  border-radius: 0.28rem;
+  background: white;
+  transform: translateY(-0.28rem);
+  position: absolute;
+  width: 100%;
+  z-index: 99;
+  .header {
+    display: flex;
+    align-items: center;
+    height: 1rem;
+    padding-left: 0.24rem;
+    .list-left,
+    .list-right {
+      display: flex;
+      align-items: center;
+    }
+    .list-left {
+      flex: 0 0 55%;
+      height: 100%;
+      border-bottom: 1px solid #ccc;
+      .span {
+        margin-left: 0.12rem;
+        color: #ccc;
+        font-size: 0.32rem;
+      }
+    }
+    .list-right {
+      flex: 0 0 45%;
+      background: var(--color);
+      height: 100%;
+      border-radius: 0.08rem;
+      color: white;
+      font-size: 0.34rem;
+      justify-content: center;
+    }
+  }
+}
+
 .rot {
   transform: rotate(90deg);
 }
 .header {
   position: relative;
-  height: 5.68rem;
+  height: 5.88rem;
   .bg {
     position: absolute;
     z-index: 1;
